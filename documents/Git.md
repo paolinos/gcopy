74f2c3e69dc16db508d798b903a84b031d07132c
Pablo Cieri <pendlejin@yahoo.com.ar>
2025-10-02 10:26:04 +0200

    test: refactor and clean tests (#2)

---
+++ documents/Git.md
@@ -0,0 +1,8 @@
+# Git Rules
+
+**Branch names**
+This project is using the git flow, so each time that we're going to commit something we're going to create a new branch that should start with: `feature/, bugfix/, hotfix/, release/, improvement/, or refactor/`
+
+**Commits**
+we're using [Conventional Commits](https://www.conventionalcommits.org/en/v1.0.0/) for this project.
+recommends `build:, chore:, ci:, docs:, style:, refactor:, perf:, test:`
--- pkg/copy/copy_test.go
+++ pkg/copy/copy_test.go
@@ -11,6 +11,7 @@ import (
 const validFilePath = "./file"
 const invalidFilePath = "unknown_file"
 
+var ErrUnexpected = errors.New("Unexpected error")
 var ErrInvalidPath = errors.New("Invalid path")
 
 type mockFile struct {
@@ -68,47 +69,33 @@ func TestCopy(t *testing.T) {
 		destinationFile.Close()
 	}()
 
-	t.Run("Given a invalid source When trying to copy Then should return an error.", func(t *testing.T) {
+	t.Run("Given a invalid source When trying to copy chunks Then should return an error.", func(t *testing.T) {
+		var ErrSourcePath = errors.New("the source path is invalid")
 		mockOpen(ErrSourcePath)
 
-		res, err := CopyFromTo(CopyOptions{
-			Source:      invalidFilePath,
-			Destination: destinationFile.Name(),
-			Override:    false,
-		})
+		err := copyChunksFromSource(invalidFilePath, destinationFile.Name())
 
 		expectError(t, err, ErrSourcePath)
-		expectCopyResult(t, res, invalidFilePath, destinationFile.Name(), 0, 0)
 	})
 
-	t.Run("Given a invalid destination When trying to copy Then should return an error.", func(t *testing.T) {
+	t.Run("Given a invalid destination When trying to copy chunks Then should return an error.", func(t *testing.T) {
+		var ErrDestinationPath = errors.New("the destination path is invalid")
 		mockOpen(sourceFile)
 		mockCreate(ErrDestinationPath)
 
-		res, err := CopyFromTo(CopyOptions{
-			Source:      sourceFile.Name(),
-			Destination: invalidFilePath,
-			Override:    false,
-		})
+		err := copyChunksFromSource(sourceFile.Name(), invalidFilePath)
 
 		expectError(t, err, ErrDestinationPath)
-		expectCopyResult(t, res, sourceFile.Name(), invalidFilePath, 0, 0)
 	})
 
 	t.Run("Given a valid source and destination When copy the file Then should return the result.", func(t *testing.T) {
 		mockOpen(sourceFile)
 		mockCreate(destinationFile)
 
-		res, err := CopyFromTo(CopyOptions{
-			Source:      sourceFile.Name(),
-			Destination: destinationFile.Name(),
-			Override:    false,
-		})
-
+		err := copyChunksFromSource(sourceFile.Name(), destinationFile.Name())
 		if err != nil {
 			t.Errorf("Found an unexpected error %s", err)
 		}
-		expectCopyResult(t, res, sourceFile.Name(), destinationFile.Name(), 1, 0)
 	})
 }
 
@@ -119,22 +106,5 @@ func expectError(t *testing.T, err error, expectedError error) {
 
 	if expectedError != err {
 		t.Error(fmt.Sprintf("Expected to have an '%s' but received:", expectedError), err)
-	}
-}
-
-func expectCopyResult(t *testing.T, res CopyResult, source string, destination string, files int, folders int) {
-	if res.GetSource() != source {
-		t.Error("Invalid source.", "expected", source, "received", res.GetSource())
-	}
-	if res.GetDestination() != destination {
-		t.Error("Invalid destination.", "expected", destination, "received", res.GetDestination())
-	}
-
-	if res.CopiedFiles() != files {
-		t.Error("Invalid copied files.", "expected", files, "received", res.CopiedFiles())
-	}
-
-	if res.CopiedFolders() != folders {
-		t.Error("Invalid copied folders.", "expected", folders, "received", res.CopiedFolders())
 	}
 }
